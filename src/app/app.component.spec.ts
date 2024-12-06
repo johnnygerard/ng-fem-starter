d5a759c578a3949052dc83c2cdd245a6b075f191
Johnny Gérard <john@jgerard.dev>
2024-12-06 20:30:12 +0100

    style: reformat code

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -24,6 +24,8 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ng-fem-starter');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Hello, ng-fem-starter',
+    );
   });
 });
